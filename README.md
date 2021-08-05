e7ae7194c2929a6ca04e41d1ccf9db24b6d491e4
Felipe Gil <felipe.gil@redspace.com>
2021-08-05 14:20:42 -0300

    Skeleton

---
+++ README.md
@@ -0,0 +1,1 @@
+# Project14-Whack-a-Penguin
---
+++ project14-Whack-a-Penguin/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  project14-Whack-a-Penguin
+//
+//  Created by Felipe Gil on 2021-08-05.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+        
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    }
+}
